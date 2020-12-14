d953b3091d5c0d369f9f3b7eb7a72b4c128e92be
kbc18a10 <kbc18a10@stu.kawahara.ac.jp>
2020-12-14 10:50:44 +0900

    commit

--- index.js
+++ index.js
@@ -8,24 +8,16 @@ var server = app.listen(3000, function(){
     console.log("Node.js is listening to PORT:" + server.address().port);
 });
 
-var addresscode = ''
-
-const parser = csv.parse((error, data) => {
-    data.forEach((element, index, array) => {
-        if(element[2] == addresscode){
-            var target = element;
-            console.log(target);
-            return;
-        }
-    })
-})
-
 app.get("/address", function(req, res, next){
-    addresscode = req.query.code;
+    var code = req.query.code;
     fs.createReadStream(__dirname + '/38EHIME.CSV')
     .pipe(iconv.decodeStream('shift_jis'))
-    .pipe(parser);
-    //console.log(parser.target);
-    //res.json(parser.target);
-
+    .pipe(csv.parse((error, data) => {
+        data.forEach((element, index, array) => {
+            if(element[2] == code){
+                res.json(element);
+                return;
+            }
+        })
+    }));
 });
